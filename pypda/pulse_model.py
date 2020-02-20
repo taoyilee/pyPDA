1e1509f465f28a70dbf2368fd485f8d67a20533d
Michael Tao-Yi Lee <michael@fraxense.ai>
2020-02-19 20:53:34 -0800

    Allow detailed parameterization

--- pypda/pulse_model.py
+++ pypda/pulse_model.py
@@ -1,6 +1,7 @@
+import itertools
 from collections import deque
 from typing import Tuple
-import itertools
+
 import numpy as np
 
 from pypda.wavelets import Waveform, TriangGaussian
@@ -8,9 +9,16 @@ from pypda.wavelets import Waveform, TriangGaussian
 
 class PulseModelRaw(Waveform):
     def __init__(self, pulse_amplitudes: Tuple[float] = (8, 3, 4, 2, 1),
-                 delta_time: Tuple[int] = (10, 10, 10, 10)):
+                 delta_time: Tuple[int] = (10, 10, 10, 10),
+                 triangle_m: Tuple[int] = (20, 20, 20, 20, 20),
+                 gaussian_m: Tuple[int] = (16, 16, 16, 16, 16),
+                 gaussian_std: Tuple[int] = (2, 2, 2, 2, 2)):
         assert len(delta_time) == len(pulse_amplitudes) - 1, "len(delta_time) must be len(pulse_heights) - 1"
-        self.wavelets = [TriangGaussian(amplitude=amplitude) for i, amplitude in enumerate(pulse_amplitudes)]
+        assert len(triangle_m) == len(pulse_amplitudes), "len(triangle_m) must be len(pulse_heights)"
+        assert len(gaussian_m) == len(pulse_amplitudes), "len(gaussian_m) must be len(pulse_heights)"
+        assert len(gaussian_std) == len(pulse_amplitudes), "len(gaussian_std) must be len(pulse_heights)"
+        self.wavelets = [TriangGaussian(amplitude=am, triang_m=tm, gaussian_m=gm, gaussian_std=gs) for
+                         am, tm, gm, gs in zip(pulse_amplitudes, triangle_m, gaussian_m, gaussian_std)]
         deque(map(lambda w, delta: w.shift(delta), self.wavelets[1:], itertools.accumulate(delta_time)))
 
     @property
